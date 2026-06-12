4f51f56ec08d113cd1515bc3a0083dbd558ffddc
Victor Otávio Ferreira <victor@ferreira.id>
2026-06-11 23:11:42 -0300

    feat: run db migrations automatically on server startup

---
+++ instrumentation.ts
@@ -0,0 +1,7 @@
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { migrate } = await import('drizzle-orm/better-sqlite3/migrator')
+    const { db } = await import('@/db')
+    migrate(db, { migrationsFolder: './db/migrations' })
+  }
+}
