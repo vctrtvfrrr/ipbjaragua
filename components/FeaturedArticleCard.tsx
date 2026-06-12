092de8e2c01a037e3b558bce3b708a58d02e9823
Victor Otávio Ferreira <victor@ferreira.id>
2026-06-11 23:08:53 -0300

    fix: minor fixes

--- components/FeaturedArticleCard.tsx
+++ components/FeaturedArticleCard.tsx
@@ -5,25 +5,23 @@ export default function FeaturedArticleCard({ article }: { article: Article | un
   if (!article) return null
 
   return (
-    <li className="my-2 w-full overflow-hidden px-2 md:w-1/3 lg:w-1/3 xl:w-1/3">
-      <Link href={`/articles/${article.slug}`}>
-        <div
-          className="relative mx-2 flex items-center justify-center overflow-hidden rounded bg-gray-300 bg-cover bg-center"
-          style={{
-            height: '260px',
-            backgroundImage: 'url(/images/featured-image.png)',
-          }}
-        >
-          <div className="absolute z-10 h-full w-full bg-black opacity-50"></div>
-          <div className="relative z-20 p-5 text-center">
-            <span className="inline-block text-xs tracking-wide text-white uppercase">Artigo</span>
-            <h2 className="my-5 font-serif text-xl font-semibold text-white">{article.title}</h2>
-            {article.author ? (
-              <span className="inline-block font-sans text-xs text-white">{article.author}</span>
-            ) : null}
-          </div>
+    <Link href={`/articles/${article.slug}`}>
+      <div
+        className="relative mx-2 flex items-center justify-center overflow-hidden rounded bg-gray-300 bg-cover bg-center"
+        style={{
+          height: '260px',
+          backgroundImage: 'url(/images/featured-image.png)',
+        }}
+      >
+        <div className="absolute z-10 h-full w-full bg-black opacity-50"></div>
+        <div className="relative z-20 p-5 text-center">
+          <span className="inline-block text-xs tracking-wide text-white uppercase">Artigo</span>
+          <h2 className="my-5 font-serif text-xl font-semibold text-white">{article.title}</h2>
+          {article.author ? (
+            <span className="inline-block font-sans text-xs text-white">{article.author}</span>
+          ) : null}
         </div>
-      </Link>
-    </li>
+      </div>
+    </Link>
   )
 }
--- db/migrations/0004_amazing_earthquake.sql
+++ db/migrations/0004_amazing_earthquake.sql
@@ -1,7 +1,1 @@
 ALTER TABLE `bulletins` ADD `edition` integer NOT NULL DEFAULT 0;
---> statement-breakpoint
-UPDATE `bulletins` SET `edition` = 68 WHERE `date` = '2026-05-24';
---> statement-breakpoint
-UPDATE `bulletins` SET `edition` = 69 WHERE `date` = '2026-05-31';
---> statement-breakpoint
-UPDATE `bulletins` SET `edition` = 70 WHERE `date` = '2026-06-07';
